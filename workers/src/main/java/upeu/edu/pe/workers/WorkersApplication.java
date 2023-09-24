98d23df3cfee1e641c30efe7eddb80158d53c01d
Gino-Elvis <ginoyujra38@gmail.com>
2023-09-24 12:08:21 -0500

    as

--- workers/src/main/java/upeu/edu/pe/workers/WorkersApplication.java
+++ workers/src/main/java/upeu/edu/pe/workers/WorkersApplication.java
@@ -2,6 +2,11 @@ package upeu.edu.pe.workers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class WorkersApplication {
@@ -10,4 +15,13 @@ public class WorkersApplication {
 		SpringApplication.run(WorkersApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI custoOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("OPEN API MICROSERVICIOS WORKERS")
+				.version("0.0.1")
+				.description("servicio web workers")
+				.termsOfService("http://swagger.io/terms")
+				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
